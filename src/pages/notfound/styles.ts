168383b0db577e8ceaceb64a22fbd328455bb95d
DIEG0FERNANDES <55159614+DIEG0FERNANDES@users.noreply.github.com>
2024-10-05 21:39:05 -0400

    q

--- src/pages/notfound/styles.ts
+++ src/pages/notfound/styles.ts
@@ -2,13 +2,16 @@ import styled from "styled-components";
 
 export const ContainerNotfound = styled.div`
   flex: 1;
-  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 export const Title = styled.h1`
   font-size: 45px;
   font-family: "Bold";
+  text-align: center;
 `;
 export const Paragrafo = styled.p`
   font-size: 15px;
   font-family: "regular";
+  text-align: center;
 `;
